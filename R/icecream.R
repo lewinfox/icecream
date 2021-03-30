e96a96c3bb1a3ca5b2c530f45105b3763fa9a40f
Lewin Appleton-Fox <lewin.appleton-fox@justice.govt.nz>
2021-03-31 09:48:26 +1300

    Finish `ic()` functionality
    Add the stack and source code inspection required when
    calling `ic()` with no arguments.

--- R/icecream.R
+++ R/icecream.R
@@ -1,11 +1,17 @@
 ic <- function(x) {
   # If `x` is missing we want to print the filename, line number and parent function
   if (missing(x)) {
-    caller_fn <- try(deparse(sys.calls()[[sys.nframe() - 1]]), silent = TRUE)
+    caller_fn <- try(sys.calls()[[sys.nframe() - 1]], silent = TRUE)
+    caller_ref <- try(sys.function(-1), silent = TRUE)
     if (inherits(caller_fn, "try-error")) {
       rlang::abort("`ic()` can only be called with no arguments from within another function")
     }
-    cli::cli_alert_info("ic| Called by: {caller_fn}")
+    srcloc <- list(
+      file = getSrcFilename(caller_ref),
+      line = getSrcLocation(caller_ref),
+      fun = deparse(caller_fn[[1]])
+    )
+    cli::cli_alert_info("ic| {.path {srcloc$file}:{srcloc$line}} in {.fn {srcloc$fun}}")
     return(invisible())
   }
   # Otherwise capture the expression, evaluate and print.
