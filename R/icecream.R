0344118e612e0e7da9013ac0aa2d812d4924354e
Lewin Appleton-Fox <lewin.appleton-fox@justice.govt.nz>
2021-04-01 10:25:35 +1300

    Capture source information
    Use adapted code from `traceback()` to capture information on
    the source file and line from which `ic()` was called.

--- R/icecream.R
+++ R/icecream.R
@@ -1,18 +1,37 @@
+#' User-friendly debug statements
+#'
+#' @param x An expression, or nothing
+#'
+#' @return If `x` is an expression, returns the result of evaluating `x`. If `x` is missing nothing
+#'   is returned.
+#'
+#' @export
+#'
+#' @examples
+#' f <- function(x) x < 0
+#'
+#' ic(f(1))
+#'
+#' ic(f(-1))
 ic <- function(x) {
   # If `x` is missing we want to print the filename, line number and parent function
   if (missing(x)) {
+    src_info <- get_source_info()
     caller_fn <- try(sys.calls()[[sys.nframe() - 1]], silent = TRUE)
-    caller_ref <- try(sys.function(-1), silent = TRUE)
     if (inherits(caller_fn, "try-error")) {
       rlang::abort("`ic()` can only be called with no arguments from within another function", "icecream-error")
     }
-    srcloc <- list(
-      file = getSrcFilename(caller_ref),
-      line = getSrcLocation(caller_ref),
-      fun = deparse(caller_fn[[1]])
-    )
+    src_info$fn <- caller_fn
     prefix <- getOption("icecream.alert.prefix", "ic|")
-    cli::cli_alert_info("{prefix} {.path {srcloc$file}:{srcloc$line}} in {.fn {srcloc$fun}}")
+    if (src_info$file == "") {
+      # If the function is defined globally then it will not have a sourceref and we will print the
+      # environment instead
+      caller_ref <- sys.function(-1)
+      ge <- capture.output(environment(caller_ref))
+      cli::cli_alert_info("{prefix} {ge}: {.fn {src_info$fn}}")
+    } else {
+      cli::cli_alert_info("{prefix} {.path {src_info$file}}:{.val {src_info$line}} in {.fn {src_info$fn}}")
+    }
     return(invisible())
   }
   # Otherwise capture the expression, evaluate and print.
@@ -26,4 +45,32 @@ ic <- function(x) {
   }
   cli::cli_alert_info("{prefix} {.var {ex}}: {str_res}")
   res
+}
+
+#' Retrieve source information from the call stack
+#'
+#' This is adapted from the source code for [traceback()].
+#'
+#' @return A list containing the source file and line from which the parent function of
+#'   `get_source_info()` was called.
+#'
+#' @keywords internal
+get_source_info <- function() {
+  n <- length(x <- .traceback(2))
+  if (n == 0L) {
+    cli::cli_alert_warning("Could not retrive source information")
+  } else {
+    for (i in 1L:n) {
+      xi <- x[[i]]
+      label <- paste0(n - i + 1L, ": ")
+      m <- length(xi)
+      srcref <- attr(xi, "srcref")
+      if (!is.null(srcref)) {
+        srcfile <- attr(srcref, "srcfile")
+        line <- srcref[1L]
+        filename <- basename(srcfile$filename)
+        return(list(line = line, file = filename))
+      }
+    }
+  }
 }
