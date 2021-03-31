25a9656fff47ccd9fd04515a1e02106b1069acd5
Lewin Appleton-Fox <lewin.appleton-fox@justice.govt.nz>
2021-04-01 10:52:55 +1300

    Rewrite `ic()` to avoid use of `get_source_info()`

--- R/icecream.R
+++ R/icecream.R
@@ -16,10 +16,28 @@
 ic <- function(x) {
   # If `x` is missing we want to print the filename, line number and parent function
   if (missing(x)) {
-    src_info <- get_source_info()
+    # We want to find out where `ic()` was called from (file and line number) and print this info.
+    # In the event that the function that called `ic()` was not defined in a file then we will
+    # display the environment it lives in instead.
+    #
+    # This code is adapted from the source of `traceback()`.
+    tb <- .traceback(1)
+    xi <- tb[[1]]
+    srcref <- attr(xi, "srcref")
+    if (is.null(srcref)) {
+      rlang::abort("Could not find source reference")
+    }
+    srcfile <- attr(srcref, "srcfile")
+    src_info <- list(
+      line = srcref[1L],
+      file = basename(srcfile$filename)
+    )
     caller_fn <- try(sys.calls()[[sys.nframe() - 1]], silent = TRUE)
     if (inherits(caller_fn, "try-error")) {
-      rlang::abort("`ic()` can only be called with no arguments from within another function", "icecream-error")
+      rlang::abort(
+        "`ic()` can only be called with no arguments from within another function",
+        "icecream-error"
+      )
     }
     src_info$fn <- caller_fn
     prefix <- getOption("icecream.alert.prefix", "ic|")
@@ -28,7 +46,7 @@ ic <- function(x) {
       # environment instead
       caller_ref <- sys.function(-1)
       ge <- capture.output(environment(caller_ref))
-      cli::cli_alert_info("{prefix} {ge}: {.fn {src_info$fn}}")
+      cli::cli_alert_info("{prefix} {ge}: {.fn {src_info$fn[[1L]]}}")
     } else {
       cli::cli_alert_info("{prefix} {.path {src_info$file}}:{.val {src_info$line}} in {.fn {src_info$fn}}")
     }
@@ -39,38 +57,10 @@ ic <- function(x) {
   ex <- deparse(rlang::quo_get_expr(q))
   res <- rlang::eval_tidy(q)
   prefix <- getOption("icecream.alert.prefix", "ic|")
-  str_res <- trimws(capture.output(str(res)))
+  str_res <- trimws(capture.output(str(res))) # For nicer printing of complex objects
   if (length(str_res) > 1) {
     str_res <- str_res[[1L]]
   }
   cli::cli_alert_info("{prefix} {.var {ex}}: {str_res}")
   res
-}
-
-#' Retrieve source information from the call stack
-#'
-#' This is adapted from the source code for [traceback()].
-#'
-#' @return A list containing the source file and line from which the parent function of
-#'   `get_source_info()` was called.
-#'
-#' @keywords internal
-get_source_info <- function() {
-  n <- length(x <- .traceback(2))
-  if (n == 0L) {
-    cli::cli_alert_warning("Could not retrive source information")
-  } else {
-    for (i in 1L:n) {
-      xi <- x[[i]]
-      label <- paste0(n - i + 1L, ": ")
-      m <- length(xi)
-      srcref <- attr(xi, "srcref")
-      if (!is.null(srcref)) {
-        srcfile <- attr(srcref, "srcfile")
-        line <- srcref[1L]
-        filename <- basename(srcfile$filename)
-        return(list(line = line, file = filename))
-      }
-    }
-  }
 }
