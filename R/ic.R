2a6dbdb9f47b1fa03b7f80122b400481e3846759
Lewin Appleton-Fox <lewin.a.f@gmail.com>
2021-04-02 16:04:41 +1300

    Rename files

---
+++ R/ic.R
@@ -0,0 +1,77 @@
+#' User-friendly debug statements
+#'
+#' @param x An expression, or nothing
+#'
+#' @return If `x` is an expression, returns the result of evaluating `x`. If `x` is missing nothing
+#'   is returned.
+#'
+#' @export
+#'
+#' @examples
+#' f <- function(x) x < 0
+#'
+#' ic(f(1))
+#'
+#' ic(f(-1))
+ic <- function(x) {
+  # Capture the input to allow us to work with the expression and value separately
+  q <- rlang::enquo(x)
+
+  # The behaviour of the function changes depending on whether input is provided or not.
+  missing_input <- rlang::quo_is_missing(q)
+
+  # In the event that icecream is totally disabled we will just return the input.
+  #
+  # TODO: This triggers evaluation of the input. Is this a problem?
+  if (isFALSE(getOption("icecream.enabled"))) {
+    if (!missing_input) {
+      return(x)
+    }
+    return(invisible())
+  }
+
+  # We need to extract the context of the call (which file and line was it called from, or which
+  # environment) in order to construct the message to print. See `ic_print()` for the formatting
+  # part.
+  ctx <- ic_get_context()
+
+  # If we have inputs then we will want the expression and value to be included in the context
+  # object as well.
+  if (!missing_input) {
+    ctx$expression <- deparse(rlang::quo_get_expr(q))
+    ctx$value <- rlang::eval_tidy(q)
+  }
+
+  # Print the output!
+  ic_print(ctx)
+
+  # Return the result
+  ctx$value
+}
+
+#' Enable or disable `ic()`
+#'
+#' These functions enable or disable the `ic()` function. While disabled `ic()` will do nothing
+#' except evaluate and return its input.
+#'
+#' These are just convenience wrappers for `options(icecream.enabled = TRUE/FALSE)` used to align
+#' the API with the [Python version](https://github.com/gruns/icecream#miscellaneous).
+#'
+#' @name enable-disable
+NULL
+
+#' @describeIn enable-disable Enable `ic()`.
+#' @export
+ic_enable <- function() {
+  old_value <- getOption("icecream.enabled")
+  options(icecream.enabled = TRUE)
+  invisible(old_value)
+}
+
+#' @describeIn enable-disable Disable `ic()`.
+#' @export
+ic_disable <- function() {
+  old_value <- getOption("icecream.enabled")
+  options(icecream.enabled = FALSE)
+  invisible(old_value)
+}
--- R/icecream.R
+++ R/icecream.R
@@ -1,103 +1,21 @@
-#' User-friendly debug statements
+#' Icecream: Never debug with `print()` again
 #'
-#' @param x An expression, or nothing
+#' @section Options:
+#' The following options can be used to control behaviour:
 #'
-#' @return If `x` is an expression, returns the result of evaluating `x`. If `x` is missing nothing
-#'   is returned.
+#' * `icecream.enabled`: Boolean. If `FALSE`, calls to `ic(foo)` simply evaluate and return `foo`.
+#'   No output is printed.
+#' * `icecream.prefix`: This is printed at the beginning of every line. Defaults to `"ic|"`.
+#' * `icecream.include.context`: Boolean. If `TRUE`, when calling `ic(foo)` the source file:line
+#'   or environment will be printed along with the expression and value. This can be useful for more
+#'   complicated debugging but produces a lot of output so is disabled by default.
+#' * `icecream.output.function`: Not implemented yet. See the
+#'   [configuration](https://github.com/gruns/icecream#configuration) section of the original
+#'   project docs for details of what it will do.
+#' * `icecream.arg.to.string.function`: Not implemented yet. See the
+#'   [configuration](https://github.com/gruns/icecream#configuration) section of the original
+#'   project docs for details of what it will do.
 #'
-#' @export
-#'
-#' @examples
-#' f <- function(x) x < 0
-#'
-#' ic(f(1))
-#'
-#' ic(f(-1))
-ic <- function(x) {
-  q <- rlang::enquo(x)
-  missing_input <- rlang::quo_is_missing(q)
-  if (isFALSE(getOption("icecream.enabled"))) {
-    if (!missing_input) {
-      return(x)
-    }
-    return(invisible())
-  }
-  ctx <- ic_get_context()
-  # If `x` is missing we want to print the filename, line number and parent function
-  if (missing_input) {
-    # We want to find out where `ic()` was called from (file and line number) and print this info.
-    # In the event that the function that called `ic()` was not defined in a file then we will
-    # display the environment it lives in instead.
-    #
-    # This code is adapted from the source of `traceback()`.
-    tb <- .traceback(1)
-    xi <- tb[[1]]
-    srcref <- attr(xi, "srcref")
-    if (is.null(srcref)) {
-      rlang::abort("Could not find source reference", "icecream-error")
-    }
-    srcfile <- attr(srcref, "srcfile")
-    src_info <- list(
-      line = srcref[1L],
-      file = basename(srcfile$filename)
-    )
-    caller_fn <- try(sys.calls()[[sys.nframe() - 1]], silent = TRUE)
-    if (inherits(caller_fn, "try-error")) {
-      rlang::abort(
-        "`ic()` can only be called with no arguments from within another function",
-        "icecream-error"
-      )
-    }
-    src_info$fn <- caller_fn
-    prefix <- getOption("icecream.prefix", "ic|")
-    if (src_info$file == "") {
-      # If the function is defined globally then it will not have a sourceref and we will print the
-      # environment instead
-      caller_ref <- sys.function(-1)
-      ge <- utils::capture.output(environment(caller_ref))
-      cli::cli_alert_info("{prefix} {ge}: {.fn {src_info$fn[[1L]]}}")
-    } else {
-      cli::cli_alert_info(
-        "{prefix} {.path {src_info$file}}:{.val {src_info$line}} in {.fn {src_info$fn[[1L]]}}"
-      )
-    }
-    return(invisible())
-  }
-  # Otherwise capture the expression, evaluate and print.
-  ex <- deparse(rlang::quo_get_expr(q))
-  res <- rlang::eval_tidy(q)
-  prefix <- getOption("icecream.prefix", "ic|")
-  str_res <- trimws(utils::capture.output(utils::str(res))) # For nicer printing of complex objects
-  if (length(str_res) > 1) {
-    str_res <- str_res[[1L]]
-  }
-  cli::cli_alert_info("{prefix} {.var {ex}}: {str_res}")
-  res
-}
-
-#' Enable or disable `ic()`
-#'
-#' These functions enable or disable the `ic()` function. While disabled `ic()` will do nothing
-#' except evaluate and return its input.
-#'
-#' These are just convenience wrappers for `options(icecream.enabled = TRUE/FALSE)` used to align
-#' the API with the [Python version](https://github.com/gruns/icecream#miscellaneous).
-#'
-#' @name enable-disable
+#' @docType package
+#' @name icecream
 NULL
-
-#' @describeIn enable-disable Enable `ic()`.
-#' @export
-ic_enable <- function() {
-  old_value <- getOption("icecream.enabled")
-  options(icecream.enabled = TRUE)
-  invisible(old_value)
-}
-
-#' @describeIn enable-disable Disable `ic()`.
-#' @export
-ic_disable <- function() {
-  old_value <- getOption("icecream.enabled")
-  options(icecream.enabled = FALSE)
-  invisible(old_value)
-}
