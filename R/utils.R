cc8bf291f8d746c937d06867fdc9a1b056f77eff
Lewin Appleton-Fox <lewin.a.f@gmail.com>
2021-04-29 12:19:03 +1200

    Document rlang re-exports to keep R CMD CHECK happy

--- R/utils.R
+++ R/utils.R
@@ -1,6 +1,14 @@
 #' Functions re-exported from rlang
 #'
+#' These are used to extract and format source references. As they are not part of the public rlang
+#' API they are reimplemented here.
+#'
+#' @param srcref A `srcref` object
+#' @param dir Directory path
+#' @param x File path
+#'
 #' @name rlang-reexports
+#' @keywords internal
 NULL
 
 #' @describeIn rlang-reexports Return source location from a srcref. If no srcref is found, return
@@ -25,7 +33,8 @@ src_loc <- function(srcref, dir = getwd()) {
   paste0(relish(file, dir = dir), ":", line, ":", column)
 }
 
-#' @describeIn rlang-reexports Tidy file paths
+#' @describeIn rlang-reexports Tidy file paths by removing instances of `dir` from `x`. Ensures that
+#'   we get a relative path for `x`.
 relish <- function(x, dir = getwd()) {
   if (substr(dir, nchar(dir), nchar(dir)) != "/") {
     dir <- paste0(dir, "/")
