76e980a227150f7018608aa76a292b9b15597886
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2021-07-21 13:42:57 +0200

    changes in docs, reorganize data.frames and lists

--- R/autopeek.R
+++ R/autopeek.R
@@ -5,24 +5,29 @@
 #' "icecream.peeking.function"
 #'
 #' @param object The object to be summarized.
-#' @param ... Other algorithms passed to methods.
+#' @param ... Other arguments passed to methods.
 #'
 #' @details This is a generic function. Default method simply calls `utils::str` function.
 #'
 #' @return A string to be printed.
 #'
 #' @seealso [utils::str()] [ic_peek()]
-#' @export
 ic_autopeek <- function(object, ...) UseMethod("ic_autopeek")
 
+#' @export
 ic_autopeek.default <- str
 
+#' @param max_summary_length Integer. Maximum length of string summarizing the object.
+#'
+#' @describeIn ic_autopeek Method for list
+#'
 #' @importFrom glue glue glue_collapse single_quote
 #' @importFrom purrr map2_chr map_chr map_int detect_index
 #' @importFrom pillar obj_sum
-ic_autopeek_list_or_data.frame <- function(object,
-                                           max_summary_length = 70,
-                                           ...) {
+#' @export
+ic_autopeek.list <- function(object,
+                             max_summary_length = 70,
+                             ...) {
   # names of columns or their index if it does not exist
   col_name <- if (is.null(names(object))) seq_along(object) else ifelse(
                                                               is.na(names(object)),
@@ -51,18 +56,28 @@ ic_autopeek_list_or_data.frame <- function(object,
   paste0(header, summary)
 }
 
-#' @param max_summary_length Integer. Maximum length of string summarizing the object.
-#'
 #' @describeIn ic_autopeek Method for list
-ic_autopeek.list <- ic_autopeek_list_or_data.frame
+#' @export
+ic_autopeek.data.frame <- ic_autopeek.list
 
-#' @describeIn ic_autopeek Method for data.frame
-ic_autopeek.data.frame <- ic_autopeek_list_or_data.frame
-
+#' Get a header of the object peeked at
+#'
+#' @param object The object peeked at.
+#' @param ... Other arguments passed to methods.
+#'
+#' @details This function is used by `ic_autopeek` to get a header of the summary of a object.
+#' It should return object's top-level class name and its dimension.
+#'
 ic_autopeek_header <- function(object, ...) UseMethod("ic_autopeek_header")
 
+#' @importFrom glue glue
+#' @export
 ic_autopeek_header.default <- function(object, ...) glue("{class(object)[[1]]}: ")
 
+#' @importFrom glue glue
+#' @export
 ic_autopeek_header.list <- function(object, ...) glue("{class(object)[[1]]} [{length(object)}]: ")
 
+#' @importFrom glue glue
+#' @export
 ic_autopeek_header.data.frame <- function(object, ...) glue("{class(object)[[1]]} [{nrow(object)} x {ncol(object)}]: ")
